a7b4028107db45eddb6f8dedff4787b9ed1e87fa
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-02-21 14:10:57 -0600

    Pagination for characters working. Fized search issue

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -10,6 +10,6 @@ class HeroesController < ApplicationController
   end
 
   def search
-    @heroes = Hero.where('name LIKE ?', "%#{params[:search_term]}%")
+    @heroes = Hero.order(:name).page(params[:page]).where('name LIKE ?', "%#{params[:search_term]}%")
   end
 end
